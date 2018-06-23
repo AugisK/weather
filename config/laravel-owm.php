4affa85d177446f23f6a0dea551ff4db752c5153
Augutis <jercau@gmail.com>
2018-06-23 09:12:57 +0300

    Initial commit

---
+++ config/laravel-owm.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'api_key' => '7105908275f8e7cc2d30247fc545779c',            // visit: http://openweathermap.org/appid#get for more info.
+    'routes_enabled' => true,   // If the routes have to be enabled.
+];
